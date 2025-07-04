4fb9ffaf2736078cb61220d1f2d5c9d1d04c069e
João Victor <joao.victor@internit.net.br>
2025-07-04 19:01:33 -0300

    car crud has been added

--- car-service/src/controllers/car.controller.ts
+++ car-service/src/controllers/car.controller.ts
@@ -5,6 +5,7 @@ import { cars } from "../db/schema";
 import { createCarSchema } from "../utils/validation";
 import fs from "fs/promises";
 import uploadToCloudinary from "../helpers/cloudinary-helper";
+import { eq } from "drizzle-orm";
 
 export const getAllCars = async (req: Request, res: Response): Promise<any> => {
   logger.info("getAllCars endpoint hit...");
@@ -33,7 +34,40 @@ export const getAllCars = async (req: Request, res: Response): Promise<any> => {
   }
 };
 
+export const getCarById = async (req: Request, res: Response): Promise<any> => {
+  logger.info("getCarById endpoint hit...");
+
+  try {
+    const { id } = req.params;
+
+    const carId = Number(id);
+
+    const results = await db.select().from(cars).where(eq(cars.id, carId));
+
+    if (!results || results.length === 0) {
+      logger.warn("Nenhum carro encontrado");
+      return res.status(404).json({
+        success: false,
+        message: "Nenhum carro encontrado",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: results,
+    });
+  } catch (error) {
+    logger.error("Erro ao buscar carros:", error);
+    res.status(500).json({
+      success: false,
+      message: "Erro interno no servidor",
+    });
+  }
+};
+
 export const createCar = async (req: Request, res: Response): Promise<any> => {
+  logger.info("createCar endpoint hit...");
+  
   try {
     const { error, value } = createCarSchema.validate(req.body, {
       abortEarly: false,
@@ -41,6 +75,7 @@ export const createCar = async (req: Request, res: Response): Promise<any> => {
     });
 
     if (error) {
+      logger.warn("Erro ao criar um carro", error);
       return res.status(400).json({
         success: false,
         message: "Erro de validação",
@@ -80,10 +115,106 @@ export const createCar = async (req: Request, res: Response): Promise<any> => {
       data: result[0],
     });
   } catch (error) {
-    console.error("Erro ao criar carro:", error);
-    return res.status(500).json({
+    logger.error("Erro ao criar carro:", error);
+    res.status(500).json({
       success: false,
       message: "Erro interno no servidor",
     });
   }
 };
+
+export const updateCar = async (req: Request, res: Response): Promise<any> => {
+  logger.info("updateCar endpoint hit...");
+  
+  try {
+    const { error, value } = createCarSchema.validate(req.body, {
+      abortEarly: false,
+      stripUnknown: true,
+    });
+
+    if (error) {
+      logger.warn('Erro ao atualizar o carro', error);
+      return res.status(400).json({
+        success: false,
+        message: "Erro de validação",
+        errors: error.details.map((detail) => detail.message),
+      });
+    }
+
+    const { id } = req.params;
+
+     const carId = Number(id);
+
+    const existingCar:any = await db.select().from(cars).where(eq(cars.id, carId));
+    if (!existingCar) {
+      return res.status(404).json({
+        success: false,
+        message: "Carro não encontrado",
+      });
+    }
+
+    let imageUrl: string | undefined;
+    if ((req.files as any)?.image_url?.[0]) {
+      const file = (req.files as any).image_url[0];
+      const uploadResult = await uploadToCloudinary(file.path);
+      imageUrl = uploadResult.url;
+      await fs.unlink(file.path);
+    } else {
+      imageUrl = existingCar.image_url;
+    }
+
+    const thumbnails: string[] = [];
+    if ((req.files as any)?.thumbnail_urls) {
+      for (const file of (req.files as any).thumbnail_urls) {
+        const uploadResult = await uploadToCloudinary(file.path);
+        thumbnails.push(uploadResult.url);
+        await fs.unlink(file.path);
+      }
+    } else {
+      thumbnails.push(...existingCar.thumbnail_urls);
+    }
+
+    const result = await db
+      .update(cars)
+      .set({
+        ...value,
+        image_url: imageUrl,
+        thumbnail_urls: thumbnails,
+      })
+      .where(eq(cars.id, carId))
+      .returning();
+
+    return res.status(200).json({
+      success: true,
+      message: "Carro atualizado com sucesso",
+      data: result[0],
+    });
+  } catch (error) {
+    logger.error("Erro ao atualizar carro:", error);
+    res.status(500).json({
+      success: false,
+      message: "Erro interno no servidor",
+    });
+  }
+};
+
+export const deleteCar = async(req: Request, res: Response): Promise<any> => {
+  logger.info('deleteCar endpoint hit...');
+  
+  try {
+    const { id } = req.params;
+
+    const carId = Number(id);
+
+    await db.delete(cars).where(eq(cars.id, carId)).returning();
+
+    logger.info(`Carro deletada com sucesso`);
+    res.status(200).json({ success: true, message: "Carro deletado com sucesso" });
+  } catch (error) {
+    logger.error("Erro ao deletar carro:", error);
+    res.status(500).json({
+      success: false,
+      message: "Erro interno no servidor",
+    });
+  }
+}
--- car-service/src/routes/car.routes.ts
+++ car-service/src/routes/car.routes.ts
@@ -1,17 +1,30 @@
 import upload from '../middleware/uploadMiddleware';
-import { createCar, getAllCars } from '../controllers/car.controller';
+import { verifyToken } from '../middleware/authMiddleware';
+import { createCar, deleteCar, getAllCars, getCarById, updateCar } from '../controllers/car.controller';
 import express from 'express';
 
 const router = express.Router();
 
 router.get("/getCars", getAllCars);
+router.get("/getCar/:id", getCarById);
 router.post(
   "/createCar",
+  verifyToken,
   upload.fields([
     { name: "image_url", maxCount: 1 },
     { name: "thumbnail_urls", maxCount: 5 },
   ]),
   createCar
 );
+router.put(
+  "/updateCar/:id",
+  verifyToken,
+  upload.fields([
+    { name: "image_url", maxCount: 1 },
+    { name: "thumbnail_urls", maxCount: 5 },
+  ]),
+  updateCar
+);
+router.delete("/deleteCar/:id", verifyToken, deleteCar);
 
 export default router;
